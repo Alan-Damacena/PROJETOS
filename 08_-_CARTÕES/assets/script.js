607e018958864e2191097074eebafef9502af2e5
Alan Damacena <alanfmdamacena@outlook.com>
2022-09-11 16:59:42 -0300

    portfólio

--- 08_-_CARTÕES/assets/script.js
+++ 08_-_CARTÕES/assets/script.js
@@ -1,68 +1,59 @@
 //Selecionando os botões, a imagem inicial e a tela(container)
-const c6 = document.getElementById("btn-01");
-const inter = document.getElementById("btn-02");
-const mercado = document.getElementById("btn-03");
-const neon = document.getElementById("btn-04");
-const nubank = document.getElementById("btn-05");
-const pagbank = document.getElementById("btn-06");
-const picpay = document.getElementById("btn-07");
-const will = document.getElementById("btn-08");
+const c6_1 = document.getElementById("btn-01");
+const c6_2 = document.getElementById("btn-02");
+const c6_3 = document.getElementById("btn-03");
+const c6_4 = document.getElementById("btn-04");
+const c6_5 = document.getElementById("btn-05");
+const c6_6 = document.getElementById("btn-06");
+const c6_7 = document.getElementById("btn-07");
+const c6_8 = document.getElementById("btn-08");
 const limpar = document.getElementById("btn-09");
 const imagem = document.getElementById("title");
 const container = document.getElementById('container')
 
 //Adicionando as trocas de imagem e de fundo de tela por meio das funções
-c6.onclick = function cartaoc6(){
+c6_1.onclick = function acqua(){
     
-    imagem.src="./assets/imagens/c6.png";
-    imagem.style.borderRadius="20px"
-    container.style.backgroundColor = "rgb(182,173,173)";
+    imagem.src="./assets/imagens/acqua.png";
+    imagem.style.borderRadius="10px"
 }
 
-inter.onclick = function cartaointer(){
-    imagem.src="./assets/imagens/inter.png";
-    imagem.style.borderRadius="20px"
-    container.style.backgroundColor = "rgb(250,172,37)";
+c6_2.onclick = function rainbow(){
+    imagem.src="./assets/imagens/rainbow.jpg";
+    imagem.style.borderRadius="10px"
 }
 
-mercado.onclick = function cartaomercado(){
-    imagem.src="./assets/imagens/mercado pago.png";
-    imagem.style.borderRadius="20px"
-    container.style.backgroundColor = "rgb(17,16,16)";
+c6_3.onclick = function gold(){
+    imagem.src="./assets/imagens/gold.jpg";
+    imagem.style.borderRadius="10px"
 }
 
-neon.onclick = function cartaoneon(){
-    imagem.src="./assets/imagens/neon.png";
-    imagem.style.borderRadius="20px"
-    container.style.backgroundColor = "rgb(54,218,241)";
+c6_4.onclick = function spice(){
+    imagem.src="./assets/imagens/spice.png";
+    imagem.style.borderRadius="10px"
 }
 
-nubank.onclick = function cartaonubank(){
-    imagem.src="./assets/imagens/nubank.png";
-    imagem.style.borderRadius="20px"
-    container.style.backgroundColor = "blueviolet";
+c6_5.onclick = function carbon(){
+    imagem.src="./assets/imagens/carbon.png";
+    imagem.style.borderRadius="10px"
 }
 
-pagbank.onclick = function cartaopagbank(){
-    imagem.src="./assets/imagens/pagbank.png";
-    imagem.style.borderRadius="20px"
-    container.style.backgroundColor = "rgb(95, 89, 89)";
+c6_6.onclick = function boreal_forest(){
+    imagem.src="./assets/imagens/boreal_forest.png";
+    imagem.style.borderRadius="10px"
 }
 
-picpay.onclick = function cartaopicpay(){
-    imagem.src="./assets/imagens/pic pay.png";
-    imagem.style.borderRadius="20px"
-    container.style.backgroundColor = "rgb(70, 170, 70)";
+c6_7.onclick = function bubblegum(){
+    imagem.src="./assets/imagens/bubblegum.jpg";
+    imagem.style.borderRadius="10px"
 }
 
-will.onclick = function cartaowillbank(){
-    imagem.src="./assets/imagens/will bank.png";
-    imagem.style.borderRadius="20px"
-    container.style.backgroundColor = "rgb(247, 247, 54)";
+c6_8.onclick = function red(){
+    imagem.src="./assets/imagens/red.jpeg";
+    imagem.style.borderRadius="10px"
 }
 
 limpar.onclick = function limpar(){
-    imagem.src="./assets/imagens/escolha.gif";
-    container.style.backgroundColor = 'white';
+    imagem.src="./assets/imagens/escolha.png";
 }
 
