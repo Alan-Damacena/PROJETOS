8e68223d82f024f5bf2bbe77f47a4c20f2ff74fb
Alan Damacena <alanfmdamacena@outlook.com>
2022-11-23 10:41:54 -0300

    portfolio

--- 14_-_LISTA_DE_TAREFAS/script.js
+++ 14_-_LISTA_DE_TAREFAS/script.js
@@ -1,2 +1,23 @@
 let input = document.getElementById("inputTarefa");
-let btnAdd = document.getElementById("btn-add")
+let btnAdd = document.getElementById("btn-add");
+let main = document.getElementById("areaLista");
+
+function addTarefa() {
+    //PEGAR O VALOR DIGTADO NO INPUT
+    let valorInput = input.value;
+
+    //SE NÃO FOR VAZIO, NEM NULO, NEM INDEFINIDO
+    if((valorInput !=="") && (valorInput !== null) && (valorInput !== undefined)){
+        let novoItem = ` <div class="item">
+        <div class="item-icone">
+            <i class="mdi mdi-check-circle"></i>
+        </div>
+        <div class="item-nome">
+            Teste de Tarefa
+        </div>
+        <div class="item-botao">
+            <button class="delete"><i class="mdi mdi-delete"> Deletar </i></button>
+        </div>
+    </div>`;
+    }
+}
